5ee8dd980dd93d0bfb715de0f266a58fb1ac9885
[vedhashni] <[vedhavel.1234@gmail.com]>
2021-07-08 12:05:34 +0530

    [Vedhashni] Updated the file to create board

---
+++ TicTacToeGameProblem/TicToeTacGame.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToeGameProblem
+{
+    class TicToeTacGame
+    {
+        static char[] arr = { '0', '0', '0', '0', '0', '0', '0', '0', '0', '0' };
+
+        public static void performance()
+        {
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    for (int i = 1; i < arr.Length; i++)
+                    {
+                        Console.WriteLine("{0}", arr[i]);
+                    }
+                    break;
+
+                default:
+                    break;
+
+            }
+        }
+    }
+}
